9ed7dcba5b33361cf62614cd207176909e2ce133
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-12-30 14:04:51 -0200

    Correção da não existência de datas na partida e gol

--- app/Goal.php
+++ app/Goal.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Goal extends Model {
     protected $table = 'goal';
     protected $fillable = ['match_id', 'team', 'scorer', 'assister'];
+    public $timestamps = false;
 
     public function match() {
         return $this->belongsTo('App\Match', 'match_id');
--- app/Match.php
+++ app/Match.php
@@ -8,24 +8,25 @@ class Match extends Model {
     protected $table = 'match';
     protected $fillable = ['week', 'tournament_id',
         'home_player_id', 'away_player_id'];
+    public $timestamps = false;
 
-    public function tournament() {
-        return $this->belongsTo('App\Tournament', 'tournament_id');
-    }
+public function tournament() {
+    return $this->belongsTo('App\Tournament', 'tournament_id');
+}
 
-    public function homePlayer() {
-        return $this->belongsTo('App\Player', 'home_player_id');
-    }
+public function homePlayer() {
+    return $this->belongsTo('App\Player', 'home_player_id');
+}
 
-    public function awayPlayer() {
-        return $this->belongsTo('App\Player', 'away_player_id');
-    }
+public function awayPlayer() {
+    return $this->belongsTo('App\Player', 'away_player_id');
+}
 
-    public function state() {
-        return $this->belongsTo(MatchState::class, 'match_state_id');
-    }
+public function state() {
+    return $this->belongsTo(MatchState::class, 'match_state_id');
+}
 
-    public function goals() {
-        return $this->hasMany('App\Goal', 'match_id');
-    }
+public function goals() {
+    return $this->hasMany('App\Goal', 'match_id');
+}
 }
